fdd84c8098f349e37646cd2ef8266764dd71fc01
blaze910 <dangogodspower@gmail.com>
2026-05-05 02:44:08 +0100

    Fixed root

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  serverExternalPackages: ['nodemailer'],
   /* config options here */
 };
 
